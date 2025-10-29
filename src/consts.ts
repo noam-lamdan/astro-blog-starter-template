bbd0edf99e5c118dd23aa89c195091cd1b77fa2c
Noam Lamdan <noamlamdan.ca@gmail.com>
2025-10-29 11:38:30 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Noam Lamdan";
+export const SITE_DESCRIPTION = "Toronto-based real estate agent Noam Lamdan has spent more than two decades helping clients with residential property transactions.";
